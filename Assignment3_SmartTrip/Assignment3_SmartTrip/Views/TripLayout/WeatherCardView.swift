3086d95e6a430f26215f6f8d8da499aef3579ac5
yufengWu <yuwu0958@uni.sydney.edu.au>
2026-05-03 15:25:46 +1000

    Add WeatherCardView UI and integrate OpenWeather API

---
+++ Assignment3_SmartTrip/Assignment3_SmartTrip/Views/TripLayout/WeatherCardView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherCardView.swift
+//  Assignment3_SmartTrip
+//
+//  Created by 吴雨峰 on 2026/5/3.
+//
+
+import SwiftUI
+
+struct WeatherCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherCardView()
+}
